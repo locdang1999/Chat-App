b00d3c9bd06c8e5c112897a90ab21d829126ec0b
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2024-01-14 20:57:36 +0700

    Done Chats
    Done Chats
    
    Co-Authored-By: Trang <69392580+chan1010@users.noreply.github.com>

--- client/src/pages/dashboard/Chats.js
+++ client/src/pages/dashboard/Chats.js
@@ -10,10 +10,17 @@ import {
   Avatar,
   Badge,
 } from "@mui/material";
-import { styled, alpha } from "@mui/material/styles";
+import { styled, alpha, useTheme } from "@mui/material/styles";
 import { CircleDashed, MagnifyingGlass, ArchiveBox } from "phosphor-react";
 import { faker } from "@faker-js/faker";
 import { ChatList } from "../../data";
+import { SimpleBarStyle } from "../../components/ScrollBar";
+
+const StyledChatBox = styled(Box)(({ theme }) => ({
+  "&:hover": {
+    cursor: "pointer",
+  },
+}));
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -45,13 +52,18 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 }));
 
 const ChatElement = ({ id, img, name, msg, time, unread, online }) => {
+  const theme = useTheme();
+
   return (
-    <Box
+    <StyledChatBox
       sx={{
         width: "100%",
         // height: 60,
         borderRadius: 1,
-        backgroundColor: "#FFFFFF",
+        backgroundColor:
+          theme.palette.mode === "light"
+            ? "#FFFFFF"
+            : theme.palette.background.paper,
       }}
       p={2}
     >
@@ -62,13 +74,17 @@ const ChatElement = ({ id, img, name, msg, time, unread, online }) => {
       >
         <Stack direction={"row"} spacing={2}>
           {/* col 1 */}
-          <StyledBadge
-            overlap="circular"
-            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            variant="dot"
-          >
+          {online ? (
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar src={faker.image.avatar()} />
+            </StyledBadge>
+          ) : (
             <Avatar src={faker.image.avatar()} />
-          </StyledBadge>
+          )}
           {/* col 2 */}
           <Stack spacing={0.3}>
             <Typography variant="subtitle2">{name}</Typography>
@@ -80,17 +96,21 @@ const ChatElement = ({ id, img, name, msg, time, unread, online }) => {
           <Typography sx={{ fontWeight: 600 }} variant="caption">
             {time}
           </Typography>
-          <Badge color="primary" badgeContent={2} />
+          <Badge
+            color="primary"
+            badgeContent={unread}
+            className="unread-count"
+          />
         </Stack>
       </Stack>
-    </Box>
+    </StyledChatBox>
   );
 };
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
   borderRadius: 20,
-  backgroundColor: alpha(theme.palette.background.paper, 1),
+  backgroundColor: alpha(theme.palette.background.default, 1),
   marginRight: theme.spacing(2),
   marginLeft: 0,
   width: "100%",
@@ -117,24 +137,27 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 const Chats = () => {
+  const theme = useTheme();
+
   return (
     <Box
       sx={{
         position: "relative",
-        height: "100vh",
+        height: "100%",
         width: 320,
-        backgroundColor: "#F8FAFF",
+        backgroundColor:
+          theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background,
         boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
       }}
     >
-      <Stack p={3} spacing={2}>
+      <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
         <Stack
           direction={"row"}
           alignItems={"center"}
           justifyContent={"space-between"}
         >
           <Typography variant="h5">Chats</Typography>
-          <IconButton>
+          <IconButton sx={{ width: "max-content" }}>
             <CircleDashed />
           </IconButton>
         </Stack>
@@ -144,27 +167,46 @@ const Chats = () => {
             <SearchIconWrapper>
               <MagnifyingGlass color="#709CE6" />
             </SearchIconWrapper>
-            <StyledInputBase placeholder="Search..." />
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ "aria-label": "search" }}
+            />
           </Search>
         </Stack>
         {/* Archive */}
         <Stack spacing={1}>
           <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
             <ArchiveBox size={24} />
-            <Button>Archive</Button>
+            <Button variant="text">Archive</Button>
           </Stack>
           <Divider />
         </Stack>
         {/* Pinned */}
-        <Stack direction={"column"}>
-          <Stack spacing={2.4}>
-            <Typography variant="subtitle2" sx={{ color: "#676767" }}>
-              Pinned
-            </Typography>
-            {ChatList.filter((el) => el.pinned).map((el) => (
-              <ChatElement {...el} />
-            ))}
-          </Stack>
+        <Stack
+          spacing={2}
+          direction={"column"}
+          sx={{ flexGrow: 1, overflow: "auto", height: "100%" }}
+        >
+          <SimpleBarStyle timeout={500} clickOnTrack={false}>
+            {/* Pinned Chats */}
+            <Stack spacing={2.4}>
+              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                Pinned
+              </Typography>
+              {ChatList.filter((el) => el.pinned).map((el, idx) => (
+                <ChatElement {...el} key={idx} />
+              ))}
+            </Stack>
+            {/* All Chats */}
+            <Stack spacing={2.4}>
+              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                All Chats
+              </Typography>
+              {ChatList.filter((el) => !el.pinned).map((el, idx) => (
+                <ChatElement {...el} key={idx} />
+              ))}
+            </Stack>
+          </SimpleBarStyle>
         </Stack>
         {/*  */}
       </Stack>
