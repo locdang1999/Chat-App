4cb2b2f894b220d8c5786b9150ede3052b689440
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2024-01-15 11:08:26 +0700

    Fix index Component > Chat and setup Conversation
    Fix index Component > Chat and setup Conversation
    
    Co-Authored-By: Trang <69392580+chan1010@users.noreply.github.com>
    Co-Authored-By: Minh Tan <148736142+minhtan7899@users.noreply.github.com>

--- client/src/pages/dashboard/GeneralApp.js
+++ client/src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
       {/* Chats */}
       <Chats />
-    </>
+      {/* Conversation */}
+      <Box sx={{ height: "100%", width: "calc(100vw - 420px)", backgroundColor: "#FFFFFF" }}>
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
