1604a9f230b09a1e73e5f6743b36bfc86845c1a6
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2024-01-23 00:30:53 +0700

    setup redux redux-toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,23 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider as ReduxProvider } from "react-redux";
 
 //Context
 import SettingsContext from "./contexts/SettingsContext";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <SettingsContext>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SettingsContext>
+      <ReduxProvider store={store}>
+        <SettingsContext>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </SettingsContext>
+      </ReduxProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
---
+++ client/src/redux/slices/app.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  sidebar: {
+    open: false,
+    type: "CONTACT", // can be CONTACT, STARRED, SHARED
+  },
+};
+
+const slice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    // Toggle Sidebar
+    toggleSidebar(state, action) {
+      state.sidebar.open = !state.sidebar.open;
+    },
+    updateSidebarType(state, action) {
+      state.sidebar.type = action.payload.type;
+    },
+  },
+});
+
+// Reducer
+export default slice.reducer;
+
+// 
