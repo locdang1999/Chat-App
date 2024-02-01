e65445d5907b3a07f4c8ce7fe8c16f509b272f49
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2024-02-01 22:47:27 +0700

    Done Header Detail-ContactInfo

--- client/src/components/SharedMessages.js
+++ client/src/components/SharedMessages.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Box, Stack, Typography, IconButton } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { CaretLeft } from "phosphor-react";
+import { UpdateSidebarType } from "../redux/slices/app";
 
 const SharedMessages = () => {
-  return <div>SharedMessages</div>;
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  return (
+    <Box sx={{ width: 320, height: "100vh" }}>
+      <Stack sx={{ height: "100%" }}>
+        <Box
+          sx={{
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+            width: "100%",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+          }}
+        >
+          <Stack
+            sx={{ height: "100%", p: 2 }}
+            direction={"row"}
+            alignItems={"center"}
+            // justifyContent={"space-between"}
+            spacing={3}
+          >
+            <IconButton
+              onClick={() => {
+                dispatch(UpdateSidebarType("CONTACT"));
+              }}
+            >
+              <CaretLeft />
+            </IconButton>
+            <Typography variant="subtitle2">Shared Messages</Typography>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default SharedMessages;
