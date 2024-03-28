9e10ca93a7f3d632f1498c31a0eedcb661f46641
Dang Gia Loc <hahagioi169@gmail.com>
2024-03-29 00:42:13 +0700

    Done UI Auth Login

--- client/src/sections/Auth/AuthSocial.js
+++ client/src/sections/Auth/AuthSocial.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Divider, Stack, IconButton } from "@mui/material";
+import { GoogleLogo, GithubLogo, TwitterLogo } from "phosphor-react";
 
 const AuthSocial = () => {
-  return <div>AuthSocial</div>;
+  return (
+    <div>
+      <Divider
+        sx={{
+          my: 2.5,
+          typography: "overline",
+          color: "text.disabled",
+          "&::before, ::after": {
+            borderTopStyle: "dashed",
+          },
+        }}
+      >
+        OR
+      </Divider>
+      <Stack direction={"row"} justifyContent={"center"} spacing={2}>
+        <IconButton>
+          <GoogleLogo color="#DF3E30" />
+        </IconButton>
+        <IconButton color="inherit">
+          <GithubLogo />
+        </IconButton>
+        <IconButton>
+          <TwitterLogo color="#1C9CEA" />
+        </IconButton>
+      </Stack>
+    </div>
+  );
 };
 
 export default AuthSocial;
